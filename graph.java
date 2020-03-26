e3f91a3bf543bc9512a41a31dad01c275a9dbe51
='Anubhav <='anubhav.kasturia16@gmail.com>
2020-03-26 22:33:47 +0530

    removeFuncs

--- graph.java
+++ graph.java
@@ -33,6 +33,8 @@ public class graph{
         addEdge(4, 6, 5);
         addEdge(5, 6, 1);
         
+        removeEdge(0, 3);
+        removeVertex(0);
         
         
     // similarly add for other edges in the same way    
@@ -54,5 +56,35 @@ public class graph{
         graph.get(u).add(new Edge(v, w));
         graph.get(v).add(new Edge(u, w));
     }
+
+    public static void removeEdge(int u,int v){
+        int i=0;
+        for(Edge e:graph.get(u)){
+            if(e.v==v){
+                break;
+            }
+        i++;
+        }
+    graph.get(u).remove(i);
+    
+    
+        int j=0;
+        for(Edge e:graph.get(v)){
+            if(e.v==u){
+            break;
+        }
+        j++;
+    }
+    graph.get(v).remove(j); 
+ }
+
+  public static void removeVertex(int u){
+      ArrayList<Edge> listcopy=graph.get(u); //ArrayList creates copy of all v in that arraylist 
+      while(listcopy.size()!=0){
+          Edge e= listcopy.get(listcopy.size()-1); //to prevent o(n2) complexity we start from end of ArrayList
+          removeEdge(u, e.v);
+
+      }
+  } 
 }
 
